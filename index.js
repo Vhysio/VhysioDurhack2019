e20a83d6d040c9b47e6a818ba5a46a2ed370489a
Ben Harries <benkelvinharries@gmail.com>
2019-11-24 00:32:14 +0000

    init() now called by voice

--- index.js
+++ index.js
@@ -149,29 +149,3 @@ function drawPose(pose) {
         tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
     }
 }
-
-window.SpeechRecognition =
-    window.webkitSpeechRecognition || window.SpeechRecognition;
-let finalTranscript = "";
-let recognition = new window.SpeechRecognition();
-recognition.interimResults = true;
-recognition.maxAlternatives = 10;
-recognition.continuous = true;
-recognition.onresult = event => {
-    let interimTranscript = "";
-    for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-        let transcript = event.results[i][0].transcript;
-        if (transcript.includes("start exercises")) {
-            init();
-        }
-        if (event.results[i].isFinal) {
-            finalTranscript += transcript;
-        } else {
-            interimTranscript += transcript;
-        }
-    }
-    speech_input.innerHTML =
-        '<i style="color:#ddd;">' + interimTranscript + "</>";
-    // finalTranscript + '<i style="color:#ddd;">' + interimTranscript + "</>";
-};
-recognition.start();
